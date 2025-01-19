00529836e4d7d2e347eeb97c9d6284df0fb17d82
Aryan Agarwal <150834823+AryanAgarwal61@users.noreply.github.com>
2025-01-19 12:01:20 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  output: 'export', // Enables static HTML export
+};
+
+module.exports = nextConfig;
